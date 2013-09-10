cf0ec8854d32f0efdfda2ed617aa4ac1b42fff5d
Dan Friedman <danielfriedman1@gmail.com>
2013-09-10 00:11:00 -0400

    add comment on percent change in benchmarking

--- Project-Euler/problem_002_benchmarks.rb
+++ Project-Euler/problem_002_benchmarks.rb
@@ -21,3 +21,4 @@ end
 #                                   user     system      total        real
 # problem_002:                 14.710000   0.030000  14.740000 ( 14.831904)
 # problem_002_refact:           0.360000   0.000000   0.360000 (  0.368928)
+# percent change (real) = 97.513%
