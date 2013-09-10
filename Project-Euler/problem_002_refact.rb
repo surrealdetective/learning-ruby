8ad6c77bc685c3a56f43e0cf028c458a1293e12a
Dan Friedman <danielfriedman1@gmail.com>
2013-09-10 17:53:29 -0400

    removed extraneous collect method in refactoring

--- Project-Euler/problem_002_refact.rb
+++ Project-Euler/problem_002_refact.rb
@@ -1,11 +1,9 @@
 # Proj Euler 002 - sum of even fibonacci numbers under 4 mil
 
 def fibonacci_upto( numbers=[1,2], ceiling=4e6 )
-  numbers.collect do |num|
     return numbers if numbers.slice(-2,2).inject(:+) >= ceiling
-    numbers << numbers.slice(-2,2).inject(:+) # for visuals: p numbers << numbers.slice(-2,2).inject(:+)
+    numbers << numbers.slice(-2,2).inject(:+) # for visuals: p 
     fibonacci_upto(numbers)
-  end
 end
 
 def sum_of_evens(array)
