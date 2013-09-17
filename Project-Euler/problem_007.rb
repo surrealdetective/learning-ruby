a6b89f191d9296a7ce1e762e007297e17ae730a5
Dan Friedman <danielfriedman1@gmail.com>
2013-09-17 11:34:53 -0400

    prob 7 solution

--- Project-Euler/problem_007.rb
+++ Project-Euler/problem_007.rb
@@ -1,15 +1,23 @@
 # By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
 # What is the 10 001st prime number?
 
+# Is it cheating to use the Prime????
+
 require 'prime'
 Inf = 1.0/0.0
 
-nth_prime = [0,1] # [1st, the prime ]
-number = 1
+# def method_name
+
+# nth_prime = [0,1] # [1st, the prime ]
+
 2.upto(Inf).each do |number|
   if number.prime?
+    # nth_prime ||= []
     nth_prime =[ nth_prime[0]+=1, number ]
     p nth_prime
   end
   break if nth_prime.first == 10_001
 end
+
+  
+# end
