6c02a9aad2adda2a035031cb51641f3ce7b559b4
Dan Friedman <danielfriedman1@gmail.com>
2013-09-18 23:39:55 -0400

    clean setup for next time

--- getting-started-with-sunlight-py-to-rb/making_a_request_02.rb
+++ getting-started-with-sunlight-py-to-rb/making_a_request_02.rb
@@ -20,16 +20,3 @@
 # Ruby Version:#
 ################
 
-require 'open-uri'
-require 'pp'
-
-query_params = { 'apikey': 'API_KEY',
-             'phrase': 'fiscal cliff' 
-           }
-
-endpoint = 'http://capitolwords.org/api/text.json'
-
-response = open(endpoint, params=query_params)
-data = response.json
-
-pprint.pprint(data)
