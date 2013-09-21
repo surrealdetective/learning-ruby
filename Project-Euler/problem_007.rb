783bbfeb26b79da1705e8884bbc97f1bbe8ba280
Dan Friedman <danielfriedman1@gmail.com>
2013-09-20 21:35:44 -0400

    edit comment

--- Project-Euler/problem_007.rb
+++ Project-Euler/problem_007.rb
@@ -1,8 +1,6 @@
 # By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
 # What is the 10 001st prime number?
 
-# Is it cheating to use the Prime????
-
 require 'prime'
 Inf = 1.0/0.0
 
