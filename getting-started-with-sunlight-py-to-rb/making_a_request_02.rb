684ba9a7a0653c29233e8ba18ba0a7599899d034
Dan Friedman <danielfriedman1@gmail.com>
2013-09-22 10:27:44 -0400

    add ruby version of lesson 02 with open-uri

--- getting-started-with-sunlight-py-to-rb/making_a_request_02.rb
+++ getting-started-with-sunlight-py-to-rb/making_a_request_02.rb
@@ -20,3 +20,21 @@
 # Ruby Version:#
 ################
 
+require "open-uri"
+require "pp"
+require 'json'
+
+# query_params = { 'apikey' => 'API_KEY', 'phrase' => 'fiscal cliff' }
+
+endpoint = 'http://capitolwords.org/api/1/text.json'
+
+response = open(endpoint + '?phrase=fiscal+cliff' + "&apikey=______your-api-key-here______").read
+
+data = JSON.parse(response)
+
+pp data
+
+# Note:
+  # the codeacademy endpoint given in the lesson is: 'http://capitolwords.org/api/text.json'
+  # the real root api is:  http://capitolwords.org/api/1/ , (official documentation there).
+  # example query to the dates.json endpoint:  http://capitolwords.org/api/1/dates.json?apikey=<YOUR_KEY>
