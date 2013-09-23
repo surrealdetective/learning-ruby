bfffdd4cbc174c4d82180357a4a378720e4ab34a
Dan Friedman <danielfriedman1@gmail.com>
2013-09-23 19:00:06 -0400

    add practice http requests for the digital divide hackathon discount codes

---
+++ digitalunidivided.rb
@@ -0,0 +1,29 @@
+# Query for Digital Undivided Hackathon Dev Discount
+# Hit http://www.digitalundivided.com/hack with the parameter of name, your name, get or post for the json object
+require 'pp'
+
+# 1)Type the query string in the url bar, maybe cheating:
+  # http://www.digitalundivided.com/hack/?name=Daniel%20Friedman
+
+# 2)With URI
+  require 'open-uri'
+  params = { 'name' => "Daniel Friedman"}
+  uri = URI.parse("http://www.digitalundivided.com/hack")
+  uri.query = URI.encode_www_form( params )
+  puts uri.open.read
+
+# 3)With Typhoeus, manually
+  require 'typhoeus'
+  endpoint = "http://www.digitalundivided.com/hack"
+  request = Typhoeus::Request.new( endpoint, params: { name: "Daniel Friedman" }).run
+  pp "\t#{request.response_body.to_s}"
+  # # => 301 to http://www.digitalundivided.com/hack/?name=Daniel%20Friedman
+  # so:
+  request2 = Typhoeus::Request.new( 'http://www.digitalundivided.com/hack/?name=Daniel%20Friedman').run
+  puts request2.response_body
+
+# 4)Typhoeus all-in-one, request-object-free
+  require 'typhoeus'
+  endpoint = "http://www.digitalundivided.com/hack"
+  request = Typhoeus.get( endpoint, params: {name: "Daniel Friedman"}, followlocation: true)
+  puts request.response_body
